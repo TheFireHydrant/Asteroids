107437f61364225a71715cda01d8f07ff964ddd9
TheFireHydrant <julian@heimdesignstudio.com>
2025-03-23 15:50:16 -0600

    fixed bug and added circleshape.py

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from constants import *
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
-        for event in pygame.get():
-            if event.type == pygame.quit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
