f2666a808d6e4157609cdc2dc71fecd3d506f110
TheFireHydrant <julian@heimdesignstudio.com>
2025-03-24 18:05:30 -0600

    added a rotateable player

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -17,6 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
